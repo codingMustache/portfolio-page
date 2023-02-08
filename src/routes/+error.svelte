870a282cc9ff2e30c3c854739c40ea970768f28a
codingMustache <codingMustache@icloud.com>
2023-02-07 21:31:03 -0600

    404 page made

---
+++ src/routes/+error.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { onMount } from 'svelte';
+	onMount(
+		console.info(
+			'%c \t You Shall Not Pass\r\n         /^\\     .\r\n    /\\   "V"\r\n   /__\\   I      O  o\r\n  //..\\\\  I     .\r\n  \\].`[/  I\r\n  /l\\/j\\  (]    .  O\r\n /. ~~ ,\\/I          .\r\n \\\\L__j^\\/I       o\r\n  \\/--v}  I     o   .\r\n  |    |  I   _________\r\n  |    |  I c(`       \')o\r\n  |    l  I   \\.     ,/\r\n_/j  L l\\_!  _//^---^\\\\_   ',
+			'color: red; font-size: 8pt; line-height: 10px'
+		)
+	);
+</script>
+
+<center>
+	<h1>You Found Yourself on the</h1>
+	<h1>Wrong Side of the</h1>
+	<h1>Web</h1>
+</center>
